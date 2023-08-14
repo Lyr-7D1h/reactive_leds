280afb1aa95f58fbb17463ef9739ac7d12958f60
Lyr <lyr-7d1h@pm.me>
2023-08-14 20:53:14 +0200

    seperated listing logic

--- src/config.py
+++ src/config.py
@@ -10,45 +10,52 @@ def int_or_str(text):
         return text
 
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument(
-    "-l",
-    "--list-devices",
-    action="store_true",
-    help="show list of audio devices and exit",
-)
+# list.
+#     "",
+#     "--list-devices",
+#     action="store_true",
+#     help="show list of audio devices and exit",
+# )
 
-config, remaining = parser.parse_known_args()
-if config.list_devices:
-    print(sd.query_devices())
-    parser.exit(0)
+# config, remaining = parser.parse_known_args()
+# if config.list_devices:
+#     print(sd.query_devices())
+#     parser.exit(0)
 
 parser = argparse.ArgumentParser(
     description=__doc__,
     formatter_class=argparse.RawDescriptionHelpFormatter,
-    parents=[parser],
+    parents=[],
 )
-parser.add_argument(
-    "channel",
+
+subparsers = parser.add_subparsers(title="Subcommands", dest="subcommand")
+
+list = subparsers.add_parser("list", help="List")
+list.add_argument("listings", choices=["serial", "output"])
+
+run = subparsers.add_parser("run", help="Run the program")
+run.add_argument(
+    "-d", "--device", type=int_or_str, help="input device (numeric ID or substring)"
+)
+run.add_argument(
+    "-s", "--serial", type=str, default="/dev/ttyUSB1", help="path to serial device"
+)
+run.add_argument(
+    "-c",
+    "--channel",
     type=int,
+    # nargs="*",
     default=1,
-    nargs="*",
     metavar="CHANNEL",
     help="input channels to plot (default: the first)",
 )
-parser.add_argument(
-    "-d", "--device", type=int_or_str, help="input device (numeric ID or substring)"
-)
-parser.add_argument(
-    "-s", "--serial", type=str, default="/dev/ttyUSB1", help="path to serial device"
-)
-parser.add_argument(
+run.add_argument(
     "--debug",
     default=False,
     help="enable debugging messages and plot data",
     action=argparse.BooleanOptionalAction,
 )
-parser.add_argument(
+run.add_argument(
     "-w",
     "--window",
     type=float,
@@ -56,11 +63,11 @@ parser.add_argument(
     metavar="DURATION",
     help="visible time slot (default: %(default)s ms)",
 )
-parser.add_argument("-b", "--blocksize", type=int, help="block size (in samples)")
-parser.add_argument(
+run.add_argument("-b", "--blocksize", type=int, help="block size (in samples)")
+run.add_argument(
     "-r", "--samplerate", type=float, help="sampling rate of audio device"
 )
-parser.add_argument(
+run.add_argument(
     "-n",
     "--downsample",
     type=int,
@@ -68,4 +75,13 @@ parser.add_argument(
     metavar="N",
     help="display every Nth sample (default: %(default)s)",
 )
-config = parser.parse_args(remaining)
+config = parser.parse_args()
+
+if config.subcommand == "list":
+    if config.listings == "output":
+        print(sd.query_devices())
+        parser.exit(0)
+    elif config.listings == "serial":
+        # FIXME: add me
+        print("Listing serials")
+        parser.exit(0)
